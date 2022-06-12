ce4cd95f90bf71647e8dfa59591a140db6b1e256
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-12 21:56:15 +0200

    docs(address.nearbyGPSCoordinate): update examples

--- src/modules/address/index.ts
+++ src/modules/address/index.ts
@@ -441,9 +441,9 @@ export class Address {
    * @param isMetric If `true` assume the radius to be in kilometers. If `false` for miles. Defaults to `false`.
    *
    * @example
-   * faker.address.nearbyGPSCoordinate() // [ 33.8475, -170.5953 ]
-   * faker.address.nearbyGPSCoordinate([33, -170]) // [ 33.0165, -170.0636 ]
-   * faker.address.nearbyGPSCoordinate([33, -170], 1000, true) // [ 37.9163, -179.2408 ]
+   * faker.address.nearbyGPSCoordinate() // [ 33.8, -170.595308 ]
+   * faker.address.nearbyGPSCoordinate([33, -170]) // [ 33.016522, -170.06367 ]
+   * faker.address.nearbyGPSCoordinate([33, -170], 1000, true) // [ 37.916567423, -179.240 ]
    */
   nearbyGPSCoordinate(
     coordinate?: [latitude: number, longitude: number],
