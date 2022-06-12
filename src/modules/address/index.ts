581bcd28ab2199779a2211db03127e3f0e047bc6
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-12 12:10:01 +0200

    chore(address): add todo

--- src/modules/address/index.ts
+++ src/modules/address/index.ts
@@ -331,6 +331,7 @@ export class Address {
    * faker.address.latitude() // -30.9501
    * faker.address.latitude(10, -10, 5) // 2.68452
    */
+  // TODO @xDivisionByZerox 2022-06-12 this signature should probably be an object for easier maintainability
   latitude(max: number = 90, min: number = -90, precision: number = 4): number {
     return this.faker.datatype.number({
       min,
@@ -350,6 +351,7 @@ export class Address {
    * faker.address.longitude() // -154.0226
    * faker.address.longitude(10, -10, 5) // -4.03620
    */
+  // TODO @xDivisionByZerox 2022-06-12 this signature should probably be an object for easier maintainability
   longitude(
     max: number = 180,
     min: number = -180,
