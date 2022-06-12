5d56f937f49888781cb9f9475a2f162df06dd1cd
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-12 12:02:42 +0200

    test(address): update precision cases

--- test/address.spec.ts
+++ test/address.spec.ts
@@ -241,7 +241,7 @@ describe('address', () => {
           expect(
             latitude.toString().split('.')[1].length,
             'The precision of latitude should be 4 digits'
-          ).toBe(4);
+          ).lessThanOrEqual(4);
 
           expect(latitude).toBeGreaterThanOrEqual(-5);
           expect(latitude).toBeLessThanOrEqual(5);
@@ -253,7 +253,7 @@ describe('address', () => {
           expect(
             latitude.toString().split('.')[1].length,
             'The precision of latitude should be 7 digits'
-          ).toBe(7);
+          ).lessThanOrEqual(7);
 
           expect(latitude).toBeGreaterThanOrEqual(-180);
           expect(latitude).toBeLessThanOrEqual(180);
@@ -280,7 +280,7 @@ describe('address', () => {
           expect(
             longitude.toString().split('.')[1].length,
             'The precision of longitude should be 4 digits'
-          ).toBe(4);
+          ).lessThanOrEqual(4);
 
           expect(longitude).toBeGreaterThanOrEqual(-30);
           expect(longitude).toBeLessThanOrEqual(100);
@@ -292,7 +292,7 @@ describe('address', () => {
           expect(
             longitude.toString().split('.')[1].length,
             'The precision of longitude should be 7 digits'
-          ).toBe(7);
+          ).lessThanOrEqual(7);
 
           expect(longitude).toBeGreaterThanOrEqual(-180);
           expect(longitude).toBeLessThanOrEqual(180);
