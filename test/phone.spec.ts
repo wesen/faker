c1e593a70d7dfdd4004aa7de7231796b8a3b821e
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-22 22:24:00 +0200

    test(phone): remove NON_SEEDED_BASED_RUN

--- test/phone.spec.ts
+++ test/phone.spec.ts
@@ -11,8 +11,6 @@ const functionNames = [
   'imei',
 ];
 
-const NON_SEEDED_BASED_RUN = 25;
-
 describe('phone', () => {
   beforeEach(() => {
     faker.locale = 'en';
@@ -48,66 +46,64 @@ describe('phone', () => {
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
   )}`, () => {
-    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
-      describe('phoneNumber()', () => {
-        it('should return a random phoneNumber with a random format', () => {
-          const phoneNumber = faker.phone.phoneNumber();
+    describe('phoneNumber()', () => {
+      it('should return a random phoneNumber with a random format', () => {
+        const phoneNumber = faker.phone.phoneNumber();
 
-          expect(phoneNumber).toMatch(/\d/);
-        });
+        expect(phoneNumber).toMatch(/\d/);
+      });
+    });
+
+    describe('number()', () => {
+      it('should return a random phoneNumber with a random format', () => {
+        const phoneNumber = faker.phone.number();
+
+        expect(phoneNumber).toMatch(/\d/);
+      });
+    });
+
+    describe('phoneNumberFormat()', () => {
+      it('should return phone number with proper US format (Array index)', () => {
+        faker.locale = 'en';
+        const phoneNumber = faker.phone.phoneNumberFormat(1);
+        expect(phoneNumber).toMatch(/\([2-9]\d\d\) [2-9]\d\d-\d\d\d\d/);
       });
 
-      describe('number()', () => {
-        it('should return a random phoneNumber with a random format', () => {
-          const phoneNumber = faker.phone.number();
-
-          expect(phoneNumber).toMatch(/\d/);
-        });
+      it('should return phone number with proper CA format (Array index)', () => {
+        faker.locale = 'en_CA';
+        const phoneNumber = faker.phone.phoneNumberFormat(1);
+        expect(phoneNumber).toMatch(/\([2-9]\d\d\)[2-9]\d\d-\d\d\d\d/);
       });
 
-      describe('phoneNumberFormat()', () => {
-        it('should return phone number with proper US format (Array index)', () => {
-          faker.locale = 'en';
-          const phoneNumber = faker.phone.phoneNumberFormat(1);
-          expect(phoneNumber).toMatch(/\([2-9]\d\d\) [2-9]\d\d-\d\d\d\d/);
-        });
+      it('should return phone number with proper PL format (Array index)', () => {
+        faker.locale = 'pl';
+        const phoneNumber = faker.phone.phoneNumberFormat(1);
+        expect(phoneNumber).toMatch(/13-\d{3}-\d{2}-\d{2}/);
+      });
+    });
 
-        it('should return phone number with proper CA format (Array index)', () => {
-          faker.locale = 'en_CA';
-          const phoneNumber = faker.phone.phoneNumberFormat(1);
-          expect(phoneNumber).toMatch(/\([2-9]\d\d\)[2-9]\d\d-\d\d\d\d/);
-        });
+    describe('phoneFormats()', () => {
+      it('should return random phone number format', () => {
+        const phoneFormat = faker.phone.phoneFormats();
+        expect(faker.definitions.phone_number.formats).contain(phoneFormat);
+      });
+    });
 
-        it('should return phone number with proper PL format (Array index)', () => {
-          faker.locale = 'pl';
-          const phoneNumber = faker.phone.phoneNumberFormat(1);
-          expect(phoneNumber).toMatch(/13-\d{3}-\d{2}-\d{2}/);
-        });
+    describe('imei()', () => {
+      it('should return a string', () => {
+        const imei = faker.phone.imei();
+        expect(imei).toBeTypeOf('string');
       });
 
-      describe('phoneFormats()', () => {
-        it('should return random phone number format', () => {
-          const phoneFormat = faker.phone.phoneFormats();
-          expect(faker.definitions.phone_number.formats).contain(phoneFormat);
-        });
+      it('should have a length of 18', () => {
+        const imei = faker.phone.imei();
+        expect(imei).toHaveLength(18);
       });
 
-      describe('imei()', () => {
-        it('should return a string', () => {
-          const imei = faker.phone.imei();
-          expect(imei).toBeTypeOf('string');
-        });
-
-        it('should have a length of 18', () => {
-          const imei = faker.phone.imei();
-          expect(imei).toHaveLength(18);
-        });
-
-        it('should be Luhn-valid', () => {
-          const imei = faker.phone.imei();
-          expect(imei).toSatisfy(luhnCheck);
-        });
+      it('should be Luhn-valid', () => {
+        const imei = faker.phone.imei();
+        expect(imei).toSatisfy(luhnCheck);
       });
-    }
+    });
   });
 });
