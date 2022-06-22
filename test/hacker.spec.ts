d80d709f0f7659e8b4628152f35539e510051158
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-22 22:22:36 +0200

    test(hacker): remove NON_SEEDED_BASED_RUN

--- test/hacker.spec.ts
+++ test/hacker.spec.ts
@@ -2,8 +2,6 @@ import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { faker } from '../src';
 import { seededRuns } from './support/seededRuns';
 
-const NON_SEEDED_BASED_RUN = 5;
-
 const functionNames = [
   'abbreviation',
   'adjective',
@@ -35,89 +33,87 @@ describe('name', () => {
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
   )}`, () => {
-    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
-      describe('abbreviation()', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
-
-        it('should return a random abbreviation from array', () => {
-          const abbreviation = faker.hacker.abbreviation();
-
-          expect(abbreviation).toBeTypeOf('string');
-          expect(abbreviation.length).toBeGreaterThan(0);
-          expect(faker.definitions.hacker.abbreviation).toContain(abbreviation);
-        });
+    describe('abbreviation()', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
       });
 
-      describe('adjective', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
+      it('should return a random abbreviation from array', () => {
+        const abbreviation = faker.hacker.abbreviation();
 
-        it('should return a random adjective from array', () => {
-          const adjective = faker.hacker.adjective();
+        expect(abbreviation).toBeTypeOf('string');
+        expect(abbreviation.length).toBeGreaterThan(0);
+        expect(faker.definitions.hacker.abbreviation).toContain(abbreviation);
+      });
+    });
 
-          expect(adjective).toBeTypeOf('string');
-          expect(adjective.length).toBeGreaterThan(0);
-          expect(faker.definitions.hacker.adjective).toContain(adjective);
-        });
+    describe('adjective', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
       });
 
-      describe('noun', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
+      it('should return a random adjective from array', () => {
+        const adjective = faker.hacker.adjective();
 
-        it('should return a random noun from array', () => {
-          const noun = faker.hacker.noun();
+        expect(adjective).toBeTypeOf('string');
+        expect(adjective.length).toBeGreaterThan(0);
+        expect(faker.definitions.hacker.adjective).toContain(adjective);
+      });
+    });
 
-          expect(noun).toBeTypeOf('string');
-          expect(noun.length).toBeGreaterThan(0);
-          expect(faker.definitions.hacker.noun).toContain(noun);
-        });
+    describe('noun', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
       });
 
-      describe('verb', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
+      it('should return a random noun from array', () => {
+        const noun = faker.hacker.noun();
 
-        it('should return a random verb from array', () => {
-          const verb = faker.hacker.verb();
+        expect(noun).toBeTypeOf('string');
+        expect(noun.length).toBeGreaterThan(0);
+        expect(faker.definitions.hacker.noun).toContain(noun);
+      });
+    });
 
-          expect(verb).toBeTypeOf('string');
-          expect(verb.length).toBeGreaterThan(0);
-          expect(faker.definitions.hacker.verb).toContain(verb);
-        });
+    describe('verb', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
       });
 
-      describe('ingverb', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
+      it('should return a random verb from array', () => {
+        const verb = faker.hacker.verb();
 
-        it('should return a random ingverb from array', () => {
-          const ingverb = faker.hacker.ingverb();
+        expect(verb).toBeTypeOf('string');
+        expect(verb.length).toBeGreaterThan(0);
+        expect(faker.definitions.hacker.verb).toContain(verb);
+      });
+    });
 
-          expect(ingverb).toBeTypeOf('string');
-          expect(ingverb.length).toBeGreaterThan(0);
-          expect(faker.definitions.hacker.ingverb).toContain(ingverb);
-        });
+    describe('ingverb', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
       });
 
-      describe('phrase', () => {
-        beforeEach(() => {
-          faker.locale = 'en';
-        });
+      it('should return a random ingverb from array', () => {
+        const ingverb = faker.hacker.ingverb();
 
-        it('should return a random phrase from array', () => {
-          const phrase = faker.hacker.phrase();
-
-          expect(phrase).toBeTypeOf('string');
-          expect(phrase.length).toBeGreaterThan(0);
-        });
+        expect(ingverb).toBeTypeOf('string');
+        expect(ingverb.length).toBeGreaterThan(0);
+        expect(faker.definitions.hacker.ingverb).toContain(ingverb);
       });
-    }
+    });
+
+    describe('phrase', () => {
+      beforeEach(() => {
+        faker.locale = 'en';
+      });
+
+      it('should return a random phrase from array', () => {
+        const phrase = faker.hacker.phrase();
+
+        expect(phrase).toBeTypeOf('string');
+        expect(phrase.length).toBeGreaterThan(0);
+      });
+    });
   });
 });
