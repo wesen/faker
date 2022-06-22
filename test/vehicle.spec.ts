0c383a08026bc4f0d95545136622eb19e6890c57
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-22 22:25:15 +0200

    test(vehicle): remove NON_SEEDED_BASED_RUN

--- test/vehicle.spec.ts
+++ test/vehicle.spec.ts
@@ -2,8 +2,6 @@ import { afterEach, describe, expect, it } from 'vitest';
 import { faker } from '../src';
 import { seededRuns } from './support/seededRuns';
 
-const NON_SEEDED_BASED_RUN = 5;
-
 const functionNames = [
   'vehicle',
   'manufacturer',
@@ -38,109 +36,105 @@ describe('vehicle', () => {
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
   )}`, () => {
-    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
-      describe('vehicle()', () => {
-        it('should return a random vehicle', () => {
-          const vehicle = faker.vehicle.vehicle();
+    describe('vehicle()', () => {
+      it('should return a random vehicle', () => {
+        const vehicle = faker.vehicle.vehicle();
 
-          expect(vehicle).toBeTruthy();
-          expect(vehicle).toBeTypeOf('string');
-          expect(vehicle.split(' ').length).toBeGreaterThanOrEqual(2);
-        });
+        expect(vehicle).toBeTruthy();
+        expect(vehicle).toBeTypeOf('string');
+        expect(vehicle.split(' ').length).toBeGreaterThanOrEqual(2);
       });
+    });
 
-      describe('manufacturer()', () => {
-        it('should return random manufacturer', () => {
-          const manufacturer = faker.vehicle.manufacturer();
+    describe('manufacturer()', () => {
+      it('should return random manufacturer', () => {
+        const manufacturer = faker.vehicle.manufacturer();
 
-          expect(manufacturer).toBeTruthy();
-          expect(manufacturer).toBeTypeOf('string');
-          expect(faker.definitions.vehicle.manufacturer).toContain(
-            manufacturer
-          );
-        });
+        expect(manufacturer).toBeTruthy();
+        expect(manufacturer).toBeTypeOf('string');
+        expect(faker.definitions.vehicle.manufacturer).toContain(manufacturer);
       });
+    });
 
-      describe('vin()', () => {
-        it('returns valid vin number', () => {
-          const vin = faker.vehicle.vin();
-          expect(vin).toMatch(
-            /^([A-HJ-NPR-Z0-9]{10}[A-HJ-NPR-Z0-9]{1}[A-HJ-NPR-Z0-9]{1}\d{5})$/
-          );
-        });
+    describe('vin()', () => {
+      it('returns valid vin number', () => {
+        const vin = faker.vehicle.vin();
+        expect(vin).toMatch(
+          /^([A-HJ-NPR-Z0-9]{10}[A-HJ-NPR-Z0-9]{1}[A-HJ-NPR-Z0-9]{1}\d{5})$/
+        );
       });
+    });
 
-      describe('model()', () => {
-        it('should return random vehicle model', () => {
-          const model = faker.vehicle.model();
+    describe('model()', () => {
+      it('should return random vehicle model', () => {
+        const model = faker.vehicle.model();
 
-          expect(model).toBeTruthy();
-          expect(model).toBeTypeOf('string');
-          expect(faker.definitions.vehicle.model).toContain(model);
-        });
+        expect(model).toBeTruthy();
+        expect(model).toBeTypeOf('string');
+        expect(faker.definitions.vehicle.model).toContain(model);
       });
+    });
 
-      describe('type()', () => {
-        it('should return random vehicle type', () => {
-          const type = faker.vehicle.type();
+    describe('type()', () => {
+      it('should return random vehicle type', () => {
+        const type = faker.vehicle.type();
 
-          expect(type).toBeTruthy();
-          expect(type).toBeTypeOf('string');
-          expect(faker.definitions.vehicle.type).toContain(type);
-        });
+        expect(type).toBeTruthy();
+        expect(type).toBeTypeOf('string');
+        expect(faker.definitions.vehicle.type).toContain(type);
       });
+    });
 
-      describe('fuel()', () => {
-        it('should return a fuel type', () => {
-          const fuel = faker.vehicle.fuel();
+    describe('fuel()', () => {
+      it('should return a fuel type', () => {
+        const fuel = faker.vehicle.fuel();
 
-          expect(fuel).toBeTruthy();
-          expect(fuel).toBeTypeOf('string');
-          expect(faker.definitions.vehicle.fuel).toContain(fuel);
-        });
+        expect(fuel).toBeTruthy();
+        expect(fuel).toBeTypeOf('string');
+        expect(faker.definitions.vehicle.fuel).toContain(fuel);
       });
+    });
 
-      describe('vin()', () => {
-        it('should return valid vin number', () => {
-          const vin = faker.vehicle.vin();
+    describe('vin()', () => {
+      it('should return valid vin number', () => {
+        const vin = faker.vehicle.vin();
 
-          expect(vin).toBeTruthy();
-          expect(vin).toBeTypeOf('string');
-          expect(vin).toMatch(
-            /^([A-HJ-NPR-Z0-9]{10}[A-HJ-NPR-Z0-9]{1}[A-HJ-NPR-Z0-9]{1}\d{5})$/
-          );
-        });
+        expect(vin).toBeTruthy();
+        expect(vin).toBeTypeOf('string');
+        expect(vin).toMatch(
+          /^([A-HJ-NPR-Z0-9]{10}[A-HJ-NPR-Z0-9]{1}[A-HJ-NPR-Z0-9]{1}\d{5})$/
+        );
       });
+    });
 
-      describe('color()', () => {
-        it('should return a random color', () => {
-          const color = faker.vehicle.color();
+    describe('color()', () => {
+      it('should return a random color', () => {
+        const color = faker.vehicle.color();
 
-          expect(color).toBeTruthy();
-          expect(color).toBeTypeOf('string');
-          expect(faker.definitions.color.human).toContain(color);
-        });
+        expect(color).toBeTruthy();
+        expect(color).toBeTypeOf('string');
+        expect(faker.definitions.color.human).toContain(color);
       });
+    });
 
-      describe('vrm()', () => {
-        it('should return a random vrm', () => {
-          const vrm = faker.vehicle.vrm();
+    describe('vrm()', () => {
+      it('should return a random vrm', () => {
+        const vrm = faker.vehicle.vrm();
 
-          expect(vrm).toBeTruthy();
-          expect(vrm).toBeTypeOf('string');
-          expect(vrm).toMatch(/^[A-Z]{2}[0-9]{2}[A-Z]{3}$/);
-        });
+        expect(vrm).toBeTruthy();
+        expect(vrm).toBeTypeOf('string');
+        expect(vrm).toMatch(/^[A-Z]{2}[0-9]{2}[A-Z]{3}$/);
       });
+    });
 
-      describe('bicycle()', () => {
-        it('should return a random type of bicycle', () => {
-          const bicycle = faker.vehicle.bicycle();
+    describe('bicycle()', () => {
+      it('should return a random type of bicycle', () => {
+        const bicycle = faker.vehicle.bicycle();
 
-          expect(bicycle).toBeTruthy();
-          expect(bicycle).toBeTypeOf('string');
-          expect(faker.definitions.vehicle.bicycle_type).toContain(bicycle);
-        });
+        expect(bicycle).toBeTruthy();
+        expect(bicycle).toBeTypeOf('string');
+        expect(faker.definitions.vehicle.bicycle_type).toContain(bicycle);
       });
-    }
+    });
   });
 });
