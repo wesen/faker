5c631e6c5d06740f1caef0218ed26b315f0ecbc0
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-22 22:25:22 +0200

    test(word): remove NON_SEEDED_BASED_RUN

--- test/word.spec.ts
+++ test/word.spec.ts
@@ -2,8 +2,6 @@ import { afterEach, describe, expect, it } from 'vitest';
 import { faker } from '../src';
 import { seededRuns } from './support/seededRuns';
 
-const NON_SEEDED_BASED_RUN = 5;
-
 const functionNames = [
   'adjective',
   'adverb',
@@ -55,76 +53,74 @@ describe('word', () => {
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
   )}`, () => {
-    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
-      describe(`adjective`, () => {
-        it('should return adjective from adjective array', () => {
-          const actual = faker.word.adjective();
+    describe(`adjective`, () => {
+      it('should return adjective from adjective array', () => {
+        const actual = faker.word.adjective();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.adjective).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.adjective).toContain(actual);
       });
+    });
 
-      describe(`adverb`, () => {
-        it('should return adverb from adverb array', () => {
-          const actual = faker.word.adverb();
+    describe(`adverb`, () => {
+      it('should return adverb from adverb array', () => {
+        const actual = faker.word.adverb();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.adverb).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.adverb).toContain(actual);
       });
+    });
 
-      describe(`conjunction`, () => {
-        it('should return conjunction from conjunction array', () => {
-          const actual = faker.word.conjunction();
+    describe(`conjunction`, () => {
+      it('should return conjunction from conjunction array', () => {
+        const actual = faker.word.conjunction();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.conjunction).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.conjunction).toContain(actual);
       });
+    });
 
-      describe(`interjection`, () => {
-        it('should return interjection from interjection array', () => {
-          const actual = faker.word.interjection();
+    describe(`interjection`, () => {
+      it('should return interjection from interjection array', () => {
+        const actual = faker.word.interjection();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.interjection).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.interjection).toContain(actual);
       });
+    });
 
-      describe(`noun`, () => {
-        it('should return noun from noun array', () => {
-          const actual = faker.word.noun();
+    describe(`noun`, () => {
+      it('should return noun from noun array', () => {
+        const actual = faker.word.noun();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.noun).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.noun).toContain(actual);
       });
+    });
 
-      describe(`preposition`, () => {
-        it('should return preposition from preposition array', () => {
-          const actual = faker.word.preposition();
+    describe(`preposition`, () => {
+      it('should return preposition from preposition array', () => {
+        const actual = faker.word.preposition();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.preposition).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.preposition).toContain(actual);
       });
+    });
 
-      describe(`verb`, () => {
-        it('should return verb from verb array', () => {
-          const actual = faker.word.verb();
+    describe(`verb`, () => {
+      it('should return verb from verb array', () => {
+        const actual = faker.word.verb();
 
-          expect(actual).toBeTruthy();
-          expect(actual).toBeTypeOf('string');
-          expect(faker.definitions.word.verb).toContain(actual);
-        });
+        expect(actual).toBeTruthy();
+        expect(actual).toBeTypeOf('string');
+        expect(faker.definitions.word.verb).toContain(actual);
       });
-    }
+    });
   });
 });
