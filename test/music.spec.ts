93e452d979573b2d860194d282270e2c9175a016
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-22 22:23:44 +0200

    test(music): remove NON_SEEDED_BASED_RUN

--- test/music.spec.ts
+++ test/music.spec.ts
@@ -4,8 +4,6 @@ import { seededRuns } from './support/seededRuns';
 
 const functionNames = ['genre', 'songName'];
 
-const NON_SEEDED_BASED_RUN = 5;
-
 describe('music', () => {
   beforeEach(() => {
     faker.locale = 'en';
@@ -28,26 +26,24 @@ describe('music', () => {
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
   )}`, () => {
-    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
-      describe('genre()', () => {
-        it('should return a genre', () => {
-          const genre = faker.music.genre();
+    describe('genre()', () => {
+      it('should return a genre', () => {
+        const genre = faker.music.genre();
 
-          expect(genre).toBeTruthy();
-          expect(genre).toBeTypeOf('string');
-          expect(faker.definitions.music.genre).toContain(genre);
-        });
+        expect(genre).toBeTruthy();
+        expect(genre).toBeTypeOf('string');
+        expect(faker.definitions.music.genre).toContain(genre);
       });
+    });
 
-      describe('songName()', () => {
-        it('returns a random song name', () => {
-          const songName = faker.music.songName();
+    describe('songName()', () => {
+      it('returns a random song name', () => {
+        const songName = faker.music.songName();
 
-          expect(songName).toBeTruthy();
-          expect(songName).toBeTypeOf('string');
-          expect(faker.definitions.music.song_name).toContain(songName);
-        });
+        expect(songName).toBeTruthy();
+        expect(songName).toBeTypeOf('string');
+        expect(faker.definitions.music.song_name).toContain(songName);
       });
-    }
+    });
   });
 });
