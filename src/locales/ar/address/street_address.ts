7c490032733d42c6f661952dfe0c0f9d94a89871
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-07-09 15:28:41 +0200

    chore(locales): ar sort files alphabetically

--- src/locales/ar/address/street_address.ts
+++ src/locales/ar/address/street_address.ts
@@ -1,4 +1,4 @@
 export default {
-  normal: '{{address.buildingNumber}} {{address.street}}',
   full: '{{address.buildingNumber}} {{address.street}} {{address.secondaryAddress}}',
+  normal: '{{address.buildingNumber}} {{address.street}}',
 };
--- src/locales/ar/name/name.ts
+++ src/locales/ar/name/name.ts
@@ -1,5 +1,5 @@
 export default [
-  '{{name.prefix}} {{name.first_name}} {{name.last_name}}',
   '{{name.first_name}} {{name.last_name}}',
   '{{name.last_name}} {{name.first_name}}',
+  '{{name.prefix}} {{name.first_name}} {{name.last_name}}',
 ];
